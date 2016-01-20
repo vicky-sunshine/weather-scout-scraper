3b606e1fbd66fd4d1e44e434fb8b5b9f183f0dc0
vicky <lch82327@gmail.com>
2016-01-21 00:16:28 +0800

    refactor scraper model

--- lib/weatherdesc/daily_weather.rb
+++ lib/weatherdesc/daily_weather.rb
@@ -8,36 +8,16 @@ module DailyWeatherScraper
     CITY_WEATHER_URL = "#{CWB_URL}/V7/forecast/taiwan/"
 
     def initialize(city)
-      @city = name_mapping(city)
-      parse_html(@city)
+      @city = name_mapping(city).delete("_")
+      parse_html(name_mapping(city))
     end
 
     def city
       @city
     end
 
-    def date
-      @data ||= parse_date
-    end
-
-    def temperature_day
-      @temperature_day ||= parse_temp_day
-    end
-
-    def temperature_night
-      @temperature_night ||= parse_temp_night
-    end
-
-    def rain_probability
-      @rain_probability ||= parse_rain_prob
-    end
-
-    def condition_day
-      @condition_day ||= parse_condition_day
-    end
-
-    def condition_night
-      @condition_night ||= parse_condition_night
+    def weathers
+      @weathers ||= parse_weathers
     end
 
     def tips
@@ -51,34 +31,18 @@ module DailyWeatherScraper
       @document = Oga.parse_html(open(url))
     end
 
-    def parse_date
+    def parse_weathers
       data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[0].children[1].children.text.split(" ")[1]
-    end
-
-    def parse_temp_day
-      data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[0].children[3].children.text
-    end
-
-    def parse_temp_night
-      data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[1].children[3].children.text
-    end
-
-    def parse_rain_prob
-      data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[0].children[9].children.text
-    end
-
-    def parse_condition_day
-      data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[0].children[5].children[1].attributes[1].value.force_encoding('UTF-8')
-    end
-
-    def parse_condition_night
-      data = @document.xpath("//table[@class='FcstBoxTable01']")
-      result = data[2].children[3].children[1].children[5].children[1].attributes[1].value.force_encoding('UTF-8')
+      data[2].children[3].children.map do |weathers|
+        timestamp= weathers.children[1].children.text.force_encoding('UTF-8')
+        temperature = weathers.children[3].children.text
+        rain_probability = weathers.children[9].children.text
+        condition = weathers.children[5].children[1].attributes[1].value.force_encoding('UTF-8')
+        [timestamp,
+         {"temperature" => temperature,
+          "rain_probability" => rain_probability,
+          "condition" => condition}]
+      end.to_h
     end
 
     def parse_tips
