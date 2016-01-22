58c5a120162b7bc8a1ab5e4160c2c8493cd36074
vicky <lch82327@gmail.com>
2016-01-22 23:22:20 +0800

    add unit test for weeklyweather

---
+++ spec/weeklyweather_spec.rb
@@ -0,0 +1,57 @@
+require 'minitest/autorun'
+require './lib/weatherscout.rb'
+
+CITY = "新竹"
+weekly_weather = WeatherScraper::WeeklyWeather.new(CITY)
+
+describe 'Weekly weather desc scrap' do
+
+  it 'should return city name' do
+    city = weekly_weather.city
+    city.must_be_instance_of String
+  end
+
+  it 'has the right number of period weathers' do
+    weathers = weekly_weather.weathers
+    weathers.size.must_equal 7
+  end
+
+  it 'should return the timestamp in correct format' do
+    timestamps = weekly_weather.weathers.keys
+    timestamps.each do |timestamp|
+      timestamp.must_match /\d\d\/\d\d.../
+    end
+  end
+
+  it 'should return day temperature' do
+    weathers = weekly_weather.weathers.values
+    weathers.each do |weather|
+      day_temperature = weather["day_temperature"]
+      day_temperature.must_match /(-|\d)\d*\s~\s(-|\d)\d*/
+    end
+  end
+
+  it 'should return night temperature' do
+    weathers = weekly_weather.weathers.values
+    weathers.each do |weather|
+      night_temperature = weather["night_temperature"]
+      night_temperature.must_match /(-|\d)\d*\s~\s(-|\d)\d*/
+    end
+  end
+
+  it 'should return day condition' do
+    weathers = weekly_weather.weathers.values
+    weathers.each do |weather|
+      day_condition = weather["day_condition"]
+      day_condition.must_be_instance_of String
+    end
+  end
+
+  it 'should return night condition' do
+    weathers = weekly_weather.weathers.values
+    weathers.each do |weather|
+      night_condition = weather["night_condition"]
+      night_condition.must_be_instance_of String
+    end
+  end
+end
